63ff4f66ab8b87096b48f011b65365b638eb4467
Jasmine Kim <jas89922@gmail.com>
2019-09-30 04:23:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,7 @@ def pioneer
 end
 
 def id_generator
-  id_generator = {
-    id: 
-  }
+  id_generator =
 end
 
 def my_hash_creator(key, value)
@@ -25,8 +23,6 @@ end
 
 def read_from_hash(hash, key)
   read_from_hash = 
-  # return the correct value using the hash and key parameters
-end
 
 def update_counting_hash(hash, key)
   update_counting_hash = 
