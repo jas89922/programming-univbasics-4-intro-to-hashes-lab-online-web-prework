c18efab93a24c996caf228892eabb5459caa72b8
Jasmine Kim <jas89922@gmail.com>
2019-09-30 04:38:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,10 +27,16 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
+  read_from_hash = {hash => key}
   # return the correct value using the hash and key parameters
 end
 
 def update_counting_hash(hash, key)
+  update_counting_hash = {
+    :parrot1 => 1,
+    :parrot2 => 2 
+    
+  }
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
