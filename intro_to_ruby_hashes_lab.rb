a4c4cd2359ab4a142deccee86933bb4f9a76d095
Jasmine Kim <jas89922@gmail.com>
2019-09-30 04:32:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,9 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
+  my_hash_creator = {
+    :key =>"value"
+  }
   # return a hash that includes the key and value parameters passed into this method
 end
 
